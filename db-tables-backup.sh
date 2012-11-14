8854b0740e6a43b9e866bab1343d8b0c70c15b4d
Josef Assad <josef@josefassad.com>
2012-11-14 11:54:43 +0100

    made the db backup script non-interactive, not requiring password

--- db-tables-backup.sh
+++ db-tables-backup.sh
@@ -11,6 +11,7 @@ for TABLE in $TABLES
 do
     mkdir -p $BACKUP_PATH/tabledumps-$TIMESTAMP
     echo "dumping data for table " $TABLE
-    pg_dump -U $DB_USER --data-only --table=$TABLE $DB_NAME > \
+    pg_dump -U $DB_USER --no-password --data-only \
+	--table=$TABLE $DB_NAME > \
 	$BACKUP_PATH/tabledumps-$TIMESTAMP/$TABLE.sql
 done
